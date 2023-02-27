7de68e15888a9bcefeba4d90ca07a20fe1e0fe99
Elle Oshiro <ekoshiro16@gmail.com>
2023-02-26 19:08:20 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+/*
+As a web developer for a publishing firm, another problem has come up. Your boss would like you to create an algorithm that can determine the difference between non-vowel and vowel characters.
+
+Write a function called characterCounter that takes in one parameter called bookText. You can expect that this parameter will always be a string representing some chunk of text from a novel. 
+
+Your function should return an object that contains 2 key-value pairs. One with a key of "numOfVowels", which is a value that is a number representing the number of vowels. And another key-value pair with a key of "numOfNonVowels", which is a value that a number representing the number of non-vowels (including spaces and other punctuation). 
+
+For example:
+
+If your function was given the string of "Hello World!", your function would return the following object:
+
+{
+    numOfVowels: 3,
+    numOfNonVowels: 9
+}
+
+Note that the number of non vowels incldues the spaces and punctuation mark characters. 
+*/
+
+// Write your code below:
